c0b25b2a6a167034d1d37c25f61f98e2bb527d13
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-10-30 23:05:37 +0700

    style: update global styles and improve layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,55 +1,54 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import { ThemeProvider } from "@/provider/theme-provider";
-import { Toaster } from "sonner";
-import { ClerkProvider } from "@clerk/nextjs";
-import {dark} from '@clerk/themes';
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
+import { ThemeProvider } from '@/provider/theme-provider'
+import { Toaster } from 'sonner'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+})
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+})
 
 export const metadata: Metadata = {
-  title: "PPT Maker",
-  description: "Create stunning presentations effortlessly with PPT Maker.",
-};
+  title: 'PPT Maker',
+  description: 'Create stunning presentations effortlessly with PPT Maker.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <ClerkProvider
-    appearance={{
-      baseTheme: dark,
-    }}>
-    <html lang="en"
-    suppressHydrationWarning
+      appearance={{
+        baseTheme: dark,
+      }}
     >
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning
-      >
-        <ThemeProvider
-        attribute={'class'}
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          suppressHydrationWarning
         >
-          {children}
-          <Toaster position="top-right" richColors />
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute={'class'}
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster position="top-right" richColors />
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
-  );
+  )
 }
