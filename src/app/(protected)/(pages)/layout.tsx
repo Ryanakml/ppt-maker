4ef87d003f771735213c46d7e21e295869ea3d4e
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-11-09 07:58:42 +0700

    adjust protected layout for consistency

--- src/app/(protected)/(pages)/layout.tsx
+++ src/app/(protected)/(pages)/layout.tsx
@@ -5,6 +5,7 @@ import {
   SidebarRail,
 } from '@/components/ui/sidebar'
 import AppSidebar from '@/components/global/app-sidebar/index'
+import UpperInfoBar from '@/components/global/upper-info-bar'
 import { redirect } from 'next/navigation'
 import React from 'react'
 import { getRecentProjects } from '@/actions/project'
@@ -15,17 +16,26 @@ type Props = {
 
 const Layout = async ({ children }: Props) => {
   const checkUser = await onAuthenticateUser()
-  const recentProjects = await getRecentProjects()
-
   if (!checkUser.user) {
     redirect('/sign-in')
   }
 
+  const recentProjectsResponse = await getRecentProjects(checkUser.user.id)
+  const recentProjects =
+    recentProjectsResponse.status === 200 &&
+    'data' in recentProjectsResponse &&
+    recentProjectsResponse.data
+      ? recentProjectsResponse.data
+      : []
+
   return (
     <SidebarProvider>
-      <AppSidebar recentProjects={[]} user={checkUser.user} />
+      <AppSidebar recentProjects={recentProjects} user={checkUser.user} />
       <SidebarRail />
-      <SidebarInset className="flex-1">{children}</SidebarInset>
+      <SidebarInset className="flex-1 flex flex-col">
+        <UpperInfoBar user={checkUser.user} />
+        <div className="p-4">{children}</div>
+      </SidebarInset>
     </SidebarProvider>
   )
 }
