21b71d626b688c81860620a2221c85fbb8a4a61b
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-11-09 07:57:26 +0700

    update constants for create-page and UI animations

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -116,3 +116,28 @@ export const themes: Theme[] = [
     navColor: '#050505',
   },
 ]
+
+export const CreatePageCard = [
+  {
+    title: 'Use a',
+    highlightedText: 'Template',
+    description: 'Write a prompt and leave everything else for us to handle',
+    type: 'template',
+    cta: 'Continue',
+  },
+  {
+    title: 'Generate with',
+    highlightedText: 'Creative AI',
+    description: 'Write a prompt and leave everything else for us to handle',
+    type: 'creative-ai',
+    highlight: true,
+    cta: 'Generate',
+  },
+  {
+    title: 'Start from',
+    highlightedText: 'Scratch',
+    description: 'Write a prompt and leave everything else for us to handle',
+    type: 'create-scratch',
+    cta: 'Continue',
+  },
+]
