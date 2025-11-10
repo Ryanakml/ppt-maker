4c2a2b60b07138078687a8f1eadf38ca293e25ac
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-11-10 20:53:36 +0700

    store: implement Zustand store for Creative AI state management

---
+++ src/store/useCreativeAIStore.tsx
@@ -0,0 +1,36 @@
+import { OutlineCard } from '@/lib/types'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+type CreativeAIStore = {
+  outlines: OutlineCard[] | []
+  addMultipleOutlines: (newOutlines: OutlineCard[]) => void
+  addOutline: (newOutline: OutlineCard) => void
+  currentAiPrompt: string
+  setCurrentAiPrompt: (prompt: string) => void
+  resetOutlines: () => void
+}
+
+const useCreativeAIStore = create<CreativeAIStore>()(
+  persist(
+    (set) => ({
+      currentAiPrompt: '',
+      setCurrentAiPrompt: (prompt: string) => set({ currentAiPrompt: prompt }),
+      outlines: [],
+      addMultipleOutlines: (newOutlines: OutlineCard[]) =>
+        set((state) => ({
+          outlines: [...state.outlines, ...newOutlines],
+        })),
+      addOutline: (newOutline: OutlineCard) =>
+        set((state) => ({
+          outlines: [...state.outlines, newOutline],
+        })),
+      resetOutlines: () => set({ outlines: [] }),
+    }),
+    {
+      name: 'creative-ai',
+    }
+  )
+)
+
+export default useCreativeAIStore
