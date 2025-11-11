f85c1fd9f1c25d8a13564cabd8ae3bfafb376330
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-11-11 22:33:49 +0700

    Refactor project actions logic

--- src/actions/project.ts
+++ src/actions/project.ts
@@ -2,6 +2,7 @@
 
 import { client } from '@/lib/prisma'
 import { onAuthenticateUser } from './user'
+import { OutlineCard } from '@/lib/types'
 
 export const getAllProjects = async () => {
   try {
@@ -162,6 +163,90 @@ export const deleteProject = async (projectId: string) => {
     return {
       status: 500,
       message: 'Internal server error',
+    }
+  }
+}
+
+export const createProject = async (title: string, outlines: OutlineCard[]) => {
+  try {
+    if (!title || !outlines || outlines.length === 0) {
+      return {
+        status: 400,
+        error: 'title and outlines are required',
+      }
+    }
+    const allOutlines = outlines.map((outline) => outline.title)
+    const checkUser = await onAuthenticateUser()
+    if (checkUser.status !== 200 || !checkUser.user) {
+      return {
+        status: 403,
+        message: 'User not authenticated',
+      }
+    }
+
+    const project = await client.project.create({
+      data: {
+        title,
+        outlines: allOutlines,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        userId: checkUser.user.id,
+      },
+    })
+
+    if (!project) {
+      return {
+        status: 500,
+        message: 'Failed to create project',
+      }
+    }
+
+    return {
+      status: 200,
+      data: project,
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      status: 500,
+      message: 'Internal server error',
+    }
+  }
+}
+
+export const getProjectById = async (projectId: string) => {
+  try {
+    const checkUser = await onAuthenticateUser()
+    if (checkUser.status !== 200 || !checkUser.user) {
+      return {
+        status: 403,
+        error: 'User not authenticate',
+      }
+    }
+
+    const project = await client.project.findFirst({
+      where: {
+        id: projectId,
+        userId: checkUser.user.id,
+      },
+    })
+
+    if (!project) {
+      return {
+        status: 404,
+        error: 'Project not found',
+      }
+    }
+
+    return {
+      status: 200,
+      data: project,
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      status: 500,
+      error: 'Internal server error',
     }
   }
 }
