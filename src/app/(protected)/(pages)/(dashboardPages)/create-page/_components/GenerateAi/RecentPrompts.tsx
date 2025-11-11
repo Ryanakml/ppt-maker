fe3f372c94ce6a825f5030898fb8aeb3c3397199
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-11-11 08:55:26 +0700

    recent-prompts: refactor state handling and improve interaction UX

--- src/app/(protected)/(pages)/(dashboardPages)/create-page/_components/GenerateAi/RecentPrompts.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/create-page/_components/GenerateAi/RecentPrompts.tsx
@@ -5,6 +5,8 @@ import React from 'react'
 import { containerVariants, itemVariants } from '@/lib/constants'
 import { Card } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
+import { timeAgo } from '@/lib/utils'
 
 type Props = {}
 
@@ -17,6 +19,8 @@ const RecentPrompts = (props: Props) => {
       setPage('creative-ai')
       addMultipleOutlines(prompt?.outlines || [])
       setCurrentAiPrompt(prompt?.title || '')
+    } else {
+      toast.error('Error', { description: 'Prompt not found' })
     }
   }
   return (
@@ -31,36 +35,37 @@ const RecentPrompts = (props: Props) => {
         variants={containerVariants}
         className="space-y-2 w-full lg:max-w-[80%] mx-auto"
       >
-        {/* {prompts.map((prompt, index) => ( */}
-        <motion.div
-          variants={containerVariants}
-          className="space-y-2 w-full lg:max-w-[80%] mx-auto"
-        >
-          <motion.div variants={itemVariants}>
-            <Card className="p-4 flex items-center justify-between hover:bg-accent/50 transition-colors duration-300 border">
-              <div className="max-w-[70%]">
-                <h3 className="font-semibold text-xl line-clamp-1">
-                  test prompt title
-                </h3>
-                <p className="font-semibold text-sm text-muted-foreground">
-                  2 days ago
-                </p>
-              </div>
-              <div className="flex items-center gap-4">
-                <span className="text-sm text-vivid">Creative Ai</span>
-                <Button
-                  variant="default"
-                  size="sm"
-                  className="rounded-xl bg-primary-20 dark:hover:bg-gray-700 hover:bg-gray-200 text-primary"
-                  onClick={() => handleEdit(prompt?.id)}
-                >
-                  Edit
-                </Button>
-              </div>
-            </Card>
+        {prompts.map((prompt, index) => (
+          <motion.div
+            key={prompt.id}
+            variants={containerVariants}
+            className="space-y-2 w-full lg:max-w-[80%] mx-auto"
+          >
+            <motion.div variants={itemVariants}>
+              <Card className="p-4 flex items-center justify-between hover:bg-accent/50 transition-colors duration-300 border">
+                <div className="max-w-[70%]">
+                  <h3 className="font-semibold text-xl line-clamp-1">
+                    {prompt?.title}
+                  </h3>
+                  <p className="font-semibold text-sm text-muted-foreground">
+                    {timeAgo(prompt?.createdAt?.toString() || '')}
+                  </p>
+                </div>
+                <div className="flex items-center gap-4">
+                  <span className="text-sm text-vivid">Creative Ai</span>
+                  <Button
+                    variant="default"
+                    size="sm"
+                    className="rounded-xl bg-primary-20 dark:hover:bg-gray-700 hover:bg-gray-200 text-primary"
+                    onClick={() => handleEdit(prompt?.id)}
+                  >
+                    Edit
+                  </Button>
+                </div>
+              </Card>
+            </motion.div>
           </motion.div>
-        </motion.div>
-        {/* }) */}
+        ))}
       </motion.div>
     </motion.div>
   )
